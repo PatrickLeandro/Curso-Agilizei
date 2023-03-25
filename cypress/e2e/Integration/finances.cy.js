8fcd1584642ca2aef24fc4c45755ec6b878e8abf
PatrickLeandro <patrickleandro34@gmail.com>
2023-03-25 13:35:15 -0300

    Update finances.cy.js

--- cypress/e2e/Integration/finances.cy.js
+++ cypress/e2e/Integration/finances.cy.js
@@ -3,12 +3,14 @@
 context('Dev Finances Agilizei', () => {
 
     /* 
-                     Hooks são:
-    Trechos de código que executão antes e depois do teste
-    before -> antes de cada teste
-    beforeEach -> antes de cada teste
-    after -> depois de todos os testes
-    afterEach -> depois de cada teste
+        Hooks são:
+            Trechos de código que executão antes e depois do teste
+
+            before -> antes de cada teste
+            beforeEach -> antes de cada teste
+            after -> depois de todos os testes
+            afterEach -> depois de cada teste
+
     */
 
     beforeEach(() =>{
@@ -16,54 +18,31 @@ context('Dev Finances Agilizei', () => {
         cy.get('#data-table tbody tr').should('have.length', 0);
     });
 
-    it.only('Cadastrar entradas', () => {
-        /* - entender o fluxo manualmente
-        - mapear os elementos que vamos interagir
-        - descrever as interações com cypress
-        - adicionar as asserções que a gente precisa */
+    it('Cadastrar entradas', () => {
+        /* 
+            - entender o fluxo manualmente
+            - mapear os elementos que vamos interagir
+            - descrever as interações com cypress
+            - adicionar as asserções que a gente precisa 
+        */
 
 
         
-        
+        // adiciona um item de entrada
         cy.get('#transaction .button').click();
         cy.get('#description').type('Mesada');
         cy.get('#amount').type(250);
         cy.get('#date').type('2023-02-17');
         cy.get('button').contains('Salvar').click();
 
-        cy.get('#data-table tbody tr').should('have.length', 1);
-
-        cy.get('#transaction .button').click();
-        cy.get('#description').type('Pizza');
-        cy.get('#amount').type(-54);
-        cy.get('#date').type('2023-02-18');
-        cy.get('button').contains('Salvar').click();
-
-        cy.get('#data-table tbody tr').should('have.length', 2)
-
-        /* cy.get('#data-table [Transaction.remove(0)]').click(); */
-        cy.get('#data-table tbody tr').first().find('img').click();
-
-        
+        // verifica se tem um item na tabela
         cy.get('#data-table tbody tr').should('have.length', 1);
 
         
-        cy.get('#data-table tbody tr').first().find('img').click();
+
+
 
         
-        cy.get('#data-table tbody tr').should('have.length', 0 );
-        Cypress.on('test:after:run', (test, runnable) => {
-            if (test.state === 'failed') {
-              // exibe um alert somente se algum teste falhar
-              alert(`Teste falhou: ${test.title}`)
-            }
-          })
-          
-          Cypress.on('run:end', () => {
-            // exibe um alert ao final de todos os testes
-            alert('Testes concluídos')
-          })
-          
         
 
 
@@ -71,19 +50,62 @@ context('Dev Finances Agilizei', () => {
 
     });
 
-    /* it('Cadastrar saidas', () => {
-        
-        cy.visit('https://devfinance-agilizei.netlify.app/')
+    it('Cadastrar saidas', () => {
 
+        // adiciona um item de saida
         cy.get('#transaction .button').click();
         cy.get('#description').type('Pizza');
         cy.get('#amount').type(-54);
         cy.get('#date').type('2023-02-18');
         cy.get('button').contains('Salvar').click();
 
+        // verifica se tem 2 itens cadastrados
+        cy.get('#data-table tbody tr').should('have.length', 1)
+    });
+
+    it('Remover entradas e saidas', () => {
+
+        cy.get('#transaction .button').click();
+        cy.get('#description').type('Mesada');
+        cy.get('#amount').type(250);
+        cy.get('#date').type('2023-02-17');
+        cy.get('button').contains('Salvar').click();
+
+        cy.get('#transaction .button').click();
+        cy.get('#description').type('Mesada');
+        cy.get('#amount').type(-50);
+        cy.get('#date').type('2023-02-17');
+        cy.get('button').contains('Salvar').click();
+
+        
+        // remove item
+        cy.get('#data-table tbody tr').first().find('img').click();
+
+        //verifica se tem um item
         cy.get('#data-table tbody tr').should('have.length', 1);
 
-    }); */
+        // remove item
+        cy.get('#data-table tbody tr').first().find('img').click();
 
+        //verifica se tirou todos os itens
+        cy.get('#data-table tbody tr').should('have.length', 0 );
+
+        
+    });
+
+    
+    // alert
+    Cypress.on('test:after:run', (test, runnable) => {
+        if (test.state === 'failed') {
+        // exibe um alert somente se algum teste falhar
+        alert(`Teste falhou: ${test.title}`)
+        }
+    })
+    
+    Cypress.on('run:end', () => {
+        // exibe um alert ao final de todos os testes
+        alert('Testes concluídos')
+    })
+  
 
 });
