2606e995cb9bacd72768f03f945efc11492e27bf
PatrickLeandro <patrickleandro34@gmail.com>
2023-03-25 10:29:11 -0300

    Update finances.cy.js

--- cypress/e2e/Integration/finances.cy.js
+++ cypress/e2e/Integration/finances.cy.js
@@ -1,15 +1,29 @@
 /// <reference types="cypress" />
 
 context('Dev Finances Agilizei', () => {
-    it('Cadastrar entradas', () => {
+
+    /* 
+                     Hooks são:
+    Trechos de código que executão antes e depois do teste
+    before -> antes de cada teste
+    beforeEach -> antes de cada teste
+    after -> depois de todos os testes
+    afterEach -> depois de cada teste
+    */
+
+    beforeEach(() =>{
+        cy.visit('https://devfinance-agilizei.netlify.app/');
+        cy.get('#data-table tbody tr').should('have.length', 0);
+    });
+
+    it.only('Cadastrar entradas', () => {
         /* - entender o fluxo manualmente
         - mapear os elementos que vamos interagir
         - descrever as interações com cypress
         - adicionar as asserções que a gente precisa */
 
 
-        cy.visit('https://devfinance-agilizei.netlify.app/')
-
+        
         
         cy.get('#transaction .button').click();
         cy.get('#description').type('Mesada');
@@ -19,10 +33,47 @@ context('Dev Finances Agilizei', () => {
 
         cy.get('#data-table tbody tr').should('have.length', 1);
 
+        cy.get('#transaction .button').click();
+        cy.get('#description').type('Pizza');
+        cy.get('#amount').type(-54);
+        cy.get('#date').type('2023-02-18');
+        cy.get('button').contains('Salvar').click();
+
+        cy.get('#data-table tbody tr').should('have.length', 2)
+
+        /* cy.get('#data-table [Transaction.remove(0)]').click(); */
+        cy.get('#data-table tbody tr').first().find('img').click();
+
+        
+        cy.get('#data-table tbody tr').should('have.length', 1);
+
+        
+        cy.get('#data-table tbody tr').first().find('img').click();
+
+        
+        cy.get('#data-table tbody tr').should('have.length', 0 );
+        Cypress.on('test:after:run', (test, runnable) => {
+            if (test.state === 'failed') {
+              // exibe um alert somente se algum teste falhar
+              alert(`Teste falhou: ${test.title}`)
+            }
+          })
+          
+          Cypress.on('run:end', () => {
+            // exibe um alert ao final de todos os testes
+            alert('Testes concluídos')
+          })
+          
+        
+
+
+
 
     });
 
-    it('Cadastrar saidas', () => {
+    /* it('Cadastrar saidas', () => {
+        
+        cy.visit('https://devfinance-agilizei.netlify.app/')
 
         cy.get('#transaction .button').click();
         cy.get('#description').type('Pizza');
@@ -30,9 +81,9 @@ context('Dev Finances Agilizei', () => {
         cy.get('#date').type('2023-02-18');
         cy.get('button').contains('Salvar').click();
 
-        cy.get('#data-table tbody tr').should('have.length', 2);
+        cy.get('#data-table tbody tr').should('have.length', 1);
 
-    });
+    }); */
 
 
 });
